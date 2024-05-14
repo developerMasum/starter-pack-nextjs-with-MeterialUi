29fe53c6e2954167640d5b2e92e92ee46cf77292
developerMasum <developer.masum2001@gmail.com>
2024-05-14 11:49:38 +0600

    added not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,32 @@
 import React from "react";
+import { Container, Typography, Box } from "@mui/material";
+import Image from "next/image";
 
 const NotFoundPage = () => {
   return (
-    <div>
-      <p> This is Not found Page </p>
-    </div>
+    <Container
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        minHeight: "100vh",
+      }}
+    >
+      <Image
+        width={400}
+        height={400}
+        src="https://www.freeparking.co.nz/learn/wp-content/uploads/2023/06/768x385-21.png" // Placeholder image URL, replace it with your desired image URL
+        alt="404 - Page Not Found"
+        style={{ marginBottom: 20 }}
+      />
+      <Typography variant="h4" gutterBottom>
+        Oops! Page Not Found
+      </Typography>
+      <Typography variant="body1" sx={{ textAlign: "center" }}>
+        The page you are looking for does not exist.
+      </Typography>
+    </Container>
   );
 };
 
--- src/components/Shared/Navbar/Navbar.tsx
+++ src/components/Shared/Navbar/Navbar.tsx
@@ -1,17 +1,19 @@
-"use client";
-import React from "react";
+"use client"
+import React, { useState } from "react";
 import {
   AppBar,
   Box,
-  Button,
   Container,
-  CssBaseline,
+  Drawer,
+  IconButton,
   Slide,
   Stack,
   Toolbar,
   Typography,
   useScrollTrigger,
 } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
 import Link from "next/link";
 
 interface Props {
@@ -33,54 +35,107 @@ function HideOnScroll(props: Props) {
 }
 
 const Navbar = () => {
-  //  const AuthButton = dynamic(
-  //    () => import("@/components/UI/AuthButton/AuthButton"),
-  //    { ssr: false }
-  //  );
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setIsDrawerOpen(!isDrawerOpen);
+  };
+
   return (
     <React.Fragment>
-      {/* <CssBaseline /> */}
       <HideOnScroll>
-        <AppBar
-          sx={{
-            background: "#F3F3F2",
-          }}
-        >
-          {/* <Toolbar> */}
-          <Container>
-            <Stack
-              py={2}
-              direction="row"
-              justifyContent="space-between"
-              alignItems={"center"}
-            >
-              <a href="/">
-                <Typography
-                  color="black"
-                  variant="h4"
-                  component="h1"
-                  fontWeight={600}
+        <AppBar sx={{ background: "#F3F3F2" }}>
+          <Toolbar>
+            <Container>
+              <Stack
+                py={2}
+                direction="row"
+                justifyContent="space-between"
+                alignItems="center"
+              >
+                <a href="/">
+                  <Typography
+                    color="black"
+                    variant="h6"
+                    component="h1"
+                    fontWeight={600}
+                  >
+                    Company
+                    <Box component="span">logo</Box>
+                    or name
+                  </Typography>
+                </a>
+                <Stack
+                  direction="row"
+                  gap={4}
+                  justifyContent="space-between"
+                  alignItems="center"
                 >
-                  Company
-                  <Box component="span">logo</Box>
-                  or name
-                </Typography>
-              </a>
-              <Stack direction={"row"} gap={4} justifyContent={"space-between"}>
-                <Typography component={Link} href={"/consultation"} passHref>
-                  item1
-                </Typography>
-                <Typography>item2</Typography>
-                <Typography>item3</Typography>
-                <Typography>item4</Typography>
-                <Typography>item5</Typography>
+                  {/* Drawer button */}
+                  <IconButton
+                    onClick={handleDrawerToggle}
+                    sx={{ display: { xs: "block", md: "none" } }}
+                  >
+                    {isDrawerOpen ? <CloseIcon /> : <MenuIcon />}
+                  </IconButton>
+                  {/* Items */}
+                  <Stack
+                    direction="row"
+                    gap={4}
+                    justifyContent="space-between"
+                    alignItems="center"
+                    sx={{ display: { xs: "none", md: "flex" } }}
+                  >
+                    <Typography
+                      component={Link}
+                      href={"/consultation"}
+                      passHref
+                    >
+                      item1
+                    </Typography>
+                    <Typography>item2</Typography>
+                    <Typography>item3</Typography>
+                    <Typography>item4</Typography>
+                    <Typography>item5</Typography>
+                  </Stack>
+                </Stack>
               </Stack>
-              {/* <AuthButton /> */}
-            </Stack>
-          </Container>
-          {/* </Toolbar> */}
+            </Container>
+          </Toolbar>
         </AppBar>
       </HideOnScroll>
+      {/* Drawer */}
+      <Drawer
+        anchor="top"
+        open={isDrawerOpen}
+        onClose={handleDrawerToggle}
+        sx={{ display: { xs: "block", md: "none" } }}
+      >
+        <Stack
+          direction="column"
+          gap={4}
+          justifyContent="center"
+          alignItems="center"
+          sx={{ width: "100vw", p: 4 }}
+        >
+          <IconButton onClick={handleDrawerToggle}>
+            <CloseIcon />
+          </IconButton>
+          {/* Items */}
+          <Typography
+            component={Link}
+            href={"/consultation"}
+            passHref
+            onClick={handleDrawerToggle}
+          >
+            item1
+          </Typography>
+          <Typography onClick={handleDrawerToggle}>item2</Typography>
+          <Typography onClick={handleDrawerToggle}>item3</Typography>
+          <Typography onClick={handleDrawerToggle}>item4</Typography>
+          <Typography onClick={handleDrawerToggle}>item5</Typography>
+        </Stack>
+      </Drawer>
     </React.Fragment>
   );
 };
